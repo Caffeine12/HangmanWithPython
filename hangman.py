f300be10a5c726deaea5c67873c052e40ebe0136
Jubair <jubairku12@gmail.com>
2024-04-27 23:16:40 +0200

    Changed appearence

--- hangman.py
+++ hangman.py
@@ -1,18 +1,26 @@
 import random
+
 # List of random words
 word_list = ['ardvark','baboon','camel']
+
 #Select a word randomly
 selected_word = random.choice(word_list)
+
 #This is for easy debugging
 print(f"psst, the word is: {selected_word}")
-#convert the selected word into a list
-selected_word_list = list(selected_word)
+
+#make a display list to give user feedback
+display=[]
+for _ in selected_word:
+    display+="_"
+print(display)
+
 #Take user guessed input
 guessed_letter=input('Guess a letter: ').lower()
 
-#match and show to user if the guess is correct or not
-for char in selected_word_list:
-    if guessed_letter==char:
-        print(guessed_letter,end=' ')
-    else:
-        print('__',end=' ')
+#match if the guess is correct or not
+for position in range(len(selected_word)):
+    if guessed_letter==selected_word[position]:
+        display[position]=guessed_letter
+    
+print(display)
