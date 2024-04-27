a6c3263fc9c2cc5ef5baa1bd5d282c3ff11ffad5
Jubair <jubairku12@gmail.com>
2024-04-28 00:48:12 +0200

    Completed Hangman Game

--- hangman.py
+++ hangman.py
@@ -9,18 +9,93 @@ selected_word = random.choice(word_list)
 #This is for easy debugging
 print(f"psst, the word is: {selected_word}")
 
+#Total lives
+lives=6
+
 #make a display list to give user feedback
 display=[]
 for _ in selected_word:
     display+="_"
 print(display)
 
-#Take user guessed input
-guessed_letter=input('Guess a letter: ').lower()
+end_of_game = False
 
-#match if the guess is correct or not
-for position in range(len(selected_word)):
-    if guessed_letter==selected_word[position]:
-        display[position]=guessed_letter
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+while not end_of_game:
+    matched=False
+    #Take user guessed input
+    guessed_letter=input('Guess a letter: ').lower()
+
+    #match if the guess is correct or not
+    for position in range(len(selected_word)):
+        if guessed_letter==selected_word[position]:
+            display[position]=guessed_letter
+            matched=True
+    if matched==False:
+        lives-=1
+        print(stages[lives])
+        if lives==0:
+            print("You have lost")
+            break
     
-print(display)
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You have won")
